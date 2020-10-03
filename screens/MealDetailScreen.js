6fb7832bc5a1842d46c94e90d09992e80766e0bf
aydevelop <ay.develop1@gmail.com>
2020-10-03 21:29:29 +0300

    Add HeaderButton

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,16 +1,43 @@
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
 import { View, Text, StyleSheet, Button } from 'react-native'
+import {
+  HeaderButtons,
+  HeaderButton,
+  Item,
+} from 'react-navigation-header-buttons'
+
+import Colors from '../constants/Colors'
+const IoniconsHeaderButton = (props) => (
+  <HeaderButton
+    {...props}
+    IconComponent={Ionicons}
+    iconSize={23}
+    color='white'
+  />
+)
 
 export const MealDetailsScreen = ({ route, navigation }) => {
   React.useLayoutEffect(() => {
     navigation.setOptions({
       title: route.params.meal.title,
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item
+            title='Favorite'
+            iconName='ios-star'
+            onPress={() => {
+              console.log('test...')
+            }}
+          />
+        </HeaderButtons>
+      ),
     })
   }, [])
 
   return (
     <View style={styles.screen}>
-      <Text>The MealDetailsScreen Screen..................</Text>
+      <Text>The MealDetailsScreen Screen.......!!!......###.....!!</Text>
       <Button
         title='Go Back'
         onPress={() => {
